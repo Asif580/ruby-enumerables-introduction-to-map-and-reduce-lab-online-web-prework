b908004c6f3106fef3cb9b558f482412266d8f80
Asiful Choudhury <asiful307@gmail.com>
2019-09-29 17:52:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,50 @@
+
 def map_to_negativize(source_array)
- i = 0
- while i < source_array do
-   puts source_array[i] 
-   i = (i + 1) * (-1)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 )
+    i += 1
+  end
+  return new
 end
+
+def map_to_no_change(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] )
+    i += 1 
+  end 
+  return new
+end
+
+
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * 2 )
+    i += 1
+  end
+  return new
+end
+
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] ** 2 )
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+  
+  
+  
+  
+  
+
